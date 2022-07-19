d5782c180bbd5059b0d0ad4b73cf319d93add723
Lucas Pereira de Lima <lucaspereiradelima2020@gmail.com>
2022-07-19 10:27:38 -0300

    Adição de mais método

--- src/com/github/lucasdevrj/frewgamesinterativo/loja/LojaVideoGame.java
+++ src/com/github/lucasdevrj/frewgamesinterativo/loja/LojaVideoGame.java
@@ -12,6 +12,16 @@ public class LojaVideoGame {
 	public void exibeMensagemBoasVindas() {
 		System.out.println("Olá, seja bem-vindo a loja " + getNome() + "!");
 		System.out.println("Estamos localizados na " + getEndereco() + ".");
+		exibeMenu();
+	}
+	
+	public void exibeMenu() {
+		System.out.println("----------|MENU PRINCIPAL|----------");
+		System.out.println("Opção 1 - Jogos.");
+		System.out.println("Opção 2 - Consoles.");
+		System.out.println("Opção 3 - Acessórios.");
+		System.out.println("Opção 4 - Comprar.");
+		System.out.println("Opção 5 - Sair.");
 	}
 	
 	public String getNome() {
