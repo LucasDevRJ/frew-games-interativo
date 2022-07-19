c2635e9be5da27300fd57b0341d8b6fde05130c9
Lucas Pereira de Lima <lucaspereiradelima2020@gmail.com>
2022-07-19 10:19:18 -0300

    Atribuindo valores os atributos

--- src/com/github/lucasdevrj/frewgamesinterativo/principal/Principal.java
+++ src/com/github/lucasdevrj/frewgamesinterativo/principal/Principal.java
@@ -1,8 +1,12 @@
 package com.github.lucasdevrj.frewgamesinterativo.principal;
 
+import com.github.lucasdevrj.frewgamesinterativo.loja.LojaVideoGame;
+
 public class Principal {
 
 	public static void main(String[] args) {
-		
+		LojaVideoGame loja = new LojaVideoGame();
+		loja.setNome("Frew Games");
+		loja.setEndereco("Rua Avaralto de Moraes - 2300");
 	}
 }
