26cdd9e61ae8ca2f009ed783a9809f056e634e17
Lucas Pereira de Lima <lucaspereiradelima2020@gmail.com>
2022-07-19 10:07:56 -0300

    Criação de classe e mais atributos

--- src/com/github/lucasdevrj/frewgamesinterativo/loja/LojaVideoGame.java
+++ src/com/github/lucasdevrj/frewgamesinterativo/loja/LojaVideoGame.java
@@ -1,15 +1,31 @@
 package com.github.lucasdevrj.frewgamesinterativo.loja;
 
+import java.util.Scanner;
+
 public class LojaVideoGame {
 
 	private String nome;
 	private String endereco;
 	
+	Scanner entrada = new Scanner(System.in);
+	
+	public void exibeMensagemBoasVindas() {
+		System.out.println("Olá, seja bem-vindo a loja ");
+	}
+	
 	public String getNome() {
 		return nome;
 	}
 	
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+	
 	public String getEndereco() {
 		return endereco;
+	}
+	
+	public void setEndereco(String endereco) {
+		this.endereco = endereco;
 	}
 }
---
+++ src/com/github/lucasdevrj/frewgamesinterativo/principal/Principal.java
@@ -0,0 +1,8 @@
+package com.github.lucasdevrj.frewgamesinterativo.principal;
+
+public class Principal {
+
+	public static void main(String[] args) {
+		
+	}
+}
