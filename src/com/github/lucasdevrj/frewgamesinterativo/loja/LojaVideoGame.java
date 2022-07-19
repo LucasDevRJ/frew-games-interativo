912082b3cfa526e3cac326c8d22a67cfef6fb1fe
Lucas Pereira de Lima <lucaspereiradelima2020@gmail.com>
2022-07-19 10:04:42 -0300

    Criação de atributos na classe LojaVideoGame

--- src/com/github/lucasdevrj/frewgamesinterativo/loja/LojaVideoGame.java
+++ src/com/github/lucasdevrj/frewgamesinterativo/loja/LojaVideoGame.java
@@ -2,4 +2,14 @@ package com.github.lucasdevrj.frewgamesinterativo.loja;
 
 public class LojaVideoGame {
 
+	private String nome;
+	private String endereco;
+	
+	public String getNome() {
+		return nome;
+	}
+	
+	public String getEndereco() {
+		return endereco;
+	}
 }
