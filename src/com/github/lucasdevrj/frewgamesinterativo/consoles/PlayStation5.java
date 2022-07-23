380886044b7bafa2e7b88574d4186f2e223db55b
Lucas Pereira de Lima <lucaspereiradelima2020@gmail.com>
2022-07-23 11:46:55 -0300

    Alteração na lista de jogos do PS5

--- src/com/github/lucasdevrj/frewgamesinterativo/consoles/PlayStation5.java
+++ src/com/github/lucasdevrj/frewgamesinterativo/consoles/PlayStation5.java
@@ -1,13 +1,154 @@
 package com.github.lucasdevrj.frewgamesinterativo.consoles;
 
+import com.github.lucasdevrj.frewgamesinterativo.jogos.Jogo;
+import com.github.lucasdevrj.frewgamesinterativo.jogos.JogoPlayStation;
+import com.github.lucasdevrj.frewgamesinterativo.loja.LojaVideoGame;
+
 public class PlayStation5 extends PlayStation {
 
 	public PlayStation5(String nome, String processador, int armazenamento, String cor, float preco) {
 		super(nome, processador, armazenamento, cor, preco);
 	}
-	
-	public static void exibeJogosPlayStation5() {
-		
-	}
 
+	public static void exibeJogosPlayStation5() {
+		System.out.println("\n----------|JOGOS DE PLAYSTATION 5|----------");
+		System.out.println("Opção 1 - Ratchet & Clank.");
+		System.out.println("Opção 2 - God of War Ragnarök.");
+		System.out.println("Opção 3 - Demon's Souls.");
+		System.out.println("Opção 4 - Uncharted: Coleção Legado Dos Ladrões.");
+		System.out.println("Opção 5 - Horizon Forbidden West.");
+		System.out.println("----------------------------------------------");
+
+		System.out.print("Digite a opção desejada: ");
+		int opcao = entrada.nextInt();
+
+		switch (opcao) {
+		case 1:
+			System.out.println("\nDeseja adicionar o jogo no carrinho?");
+			System.out.println("Opção 1 - Sim.");
+			System.out.println("Opção 2 - Volta para o menu principal.");
+			System.out.print("Digite sua opção: ");
+			opcao = entrada.nextInt();
+
+			if (opcao == 1) {
+				System.out.println("\nJogo adicionado no carrinho!");
+				Jogo jogo = new JogoPlayStation("Marvel's Spider-Man: Miles Morales", "12/11/2020", "Ação e Aventura",
+						"Insomniac Games", 120.00f);
+				LojaVideoGame.getJogos().add(jogo);
+				LojaVideoGame.exibeMenu();
+
+			} else if (opcao == 2) {
+				LojaVideoGame.exibeMenu();
+
+			} else {
+				System.out.println("Opção inválida!");
+				PlayStation4.exibeJogosPlayStation4();
+			}
+
+			break;
+
+		case 2:
+			System.out.println("\nDeseja adicionar o jogo no carrinho?");
+			System.out.println("Opção 1 - Sim.");
+			System.out.println("Opção 2 - Volta para o menu principal.");
+			System.out.print("Digite sua opção: ");
+			opcao = entrada.nextInt();
+
+			if (opcao == 1) {
+				System.out.println("\nJogo adicionado no carrinho!");
+				Jogo jogo = new JogoPlayStation("The Last of Us Part II", "19/06/2020", "Ação e Aventura",
+						"NetherRealm Studios", 120.00f);
+				LojaVideoGame.getJogos().add(jogo);
+				LojaVideoGame.exibeMenu();
+
+			} else if (opcao == 2) {
+				LojaVideoGame.exibeMenu();
+
+			} else {
+				System.out.println("Opção inválida!");
+				PlayStation4.exibeJogosPlayStation4();
+			}
+
+			break;
+
+		case 3:
+
+			System.out.println("\nDeseja adicionar o jogo no carrinho?");
+			System.out.println("Opção 1 - Sim.");
+			System.out.println("Opção 2 - Volta para o menu principal.");
+			System.out.print("Digite sua opção: ");
+			opcao = entrada.nextInt();
+
+			if (opcao == 1) {
+				System.out.println("\nJogo adicionado no carrinho!");
+				Jogo jogo = new JogoPlayStation("Horizon Zero Dawn Complete Edition Hits", "28/02/2017", "Mundo Aberto",
+						"Guerrilla Games", 110.00f);
+				LojaVideoGame.getJogos().add(jogo);
+				LojaVideoGame.exibeMenu();
+
+			} else if (opcao == 2) {
+				LojaVideoGame.exibeMenu();
+
+			} else {
+				System.out.println("Opção inválida!");
+				PlayStation4.exibeJogosPlayStation4();
+			}
+
+			break;
+
+		case 4:
+
+			System.out.println("\nDeseja adicionar o jogo no carrinho?");
+			System.out.println("Opção 1 - Sim.");
+			System.out.println("Opção 2 - Volta para o menu principal.");
+			System.out.print("Digite sua opção: ");
+			opcao = entrada.nextInt();
+
+			if (opcao == 1) {
+				System.out.println("\nJogo adicionado no carrinho!");
+				Jogo jogo = new JogoPlayStation("JRed Dead Redemption 2", "05/07/2008", "Mundo Aberto",
+						"Rockstar Games", 130.00f);
+				LojaVideoGame.getJogos().add(jogo);
+				LojaVideoGame.exibeMenu();
+
+			} else if (opcao == 2) {
+				LojaVideoGame.exibeMenu();
+
+			} else {
+				System.out.println("Opção inválida!");
+				PlayStation4.exibeJogosPlayStation4();
+			}
+
+			break;
+
+		case 5:
+
+			System.out.println("\nDeseja adicionar o jogo no carrinho?");
+			System.out.println("Opção 1 - Sim.");
+			System.out.println("Opção 2 - Volta para o menu principal.");
+			System.out.print("Digite sua opção: ");
+			opcao = entrada.nextInt();
+
+			if (opcao == 1) {
+				System.out.println("\nJogo adicionado no carrinho!");
+				Jogo jogo = new JogoPlayStation("Days Gone", "26/04/2019", "Mundo Aberto", "Bend Studio", 95.00f);
+				LojaVideoGame.getJogos().add(jogo);
+				LojaVideoGame.exibeMenu();
+
+			} else if (opcao == 2) {
+				LojaVideoGame.exibeMenu();
+
+			} else {
+				System.out.println("Opção inválida!");
+				PlayStation4.exibeJogosPlayStation4();
+			}
+
+			break;
+
+		default:
+			System.out.println("Opção inválida!");
+			PlayStation4.exibeJogosPlayStation4();
+			break;
+		}
+	}
 }
