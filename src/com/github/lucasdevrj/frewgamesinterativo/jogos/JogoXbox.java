bce944ce03a396f6f279a760b3fb49313b1ba40a
Lucas Pereira de Lima <lucaspereiradelima2020@gmail.com>
2022-07-24 20:45:47 -0300

    Adição da classe JogoXbox

---
+++ src/com/github/lucasdevrj/frewgamesinterativo/jogos/JogoXbox.java
@@ -0,0 +1,8 @@
+package com.github.lucasdevrj.frewgamesinterativo.jogos;
+
+public class JogoXbox extends Jogo {
+
+	public JogoXbox(int codigo, String nome, String modelo, String dataLancamento, String marca, float preco) {
+		super(codigo, nome, modelo, dataLancamento, marca, preco);
+	}
+}
