f48aa99fe8fac609a6d852c6de5d2aeff05342a1
Lucas Pereira de Lima <lucaspereiradelima2020@gmail.com>
2022-07-24 20:52:25 -0300

    Criação da classe Xbox360

---
+++ src/com/github/lucasdevrj/frewgamesinterativo/consoles/Xbox360.java
@@ -0,0 +1,9 @@
+package com.github.lucasdevrj.frewgamesinterativo.consoles;
+
+public class Xbox360 extends Xbox {
+
+	public Xbox360(int codigo, String nome, String modelo, String dataLancamento, String marca, float preco) {
+		super(codigo, nome, modelo, dataLancamento, marca, preco);
+	}
+
+}
