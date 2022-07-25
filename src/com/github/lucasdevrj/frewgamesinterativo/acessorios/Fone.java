78f8b18b31c9a2897d6687afa6c37544eb138349
Lucas Pereira de Lima <lucaspereiradelima2020@gmail.com>
2022-07-25 09:48:17 -0300

    Criação da classe Fone

---
+++ src/com/github/lucasdevrj/frewgamesinterativo/acessorios/Fone.java
@@ -0,0 +1,8 @@
+package com.github.lucasdevrj.frewgamesinterativo.acessorios;
+
+public class Fone extends Acessorio {
+
+	public Fone(int codigo, String nome, String modelo, String dataLancamento, String marca, float preco) {
+		super(codigo, nome, modelo, dataLancamento, marca, preco);
+	}
+}
