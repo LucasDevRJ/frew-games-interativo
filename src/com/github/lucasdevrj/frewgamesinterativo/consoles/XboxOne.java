2052090f7094175e4cab8a86a24b3abebed4eb84
Lucas Pereira de Lima <lucaspereiradelima2020@gmail.com>
2022-07-26 09:20:45 -0300

    Alteração na lista de jogos do Xbox One

--- src/com/github/lucasdevrj/frewgamesinterativo/consoles/XboxOne.java
+++ src/com/github/lucasdevrj/frewgamesinterativo/consoles/XboxOne.java
@@ -1,5 +1,9 @@
 package com.github.lucasdevrj.frewgamesinterativo.consoles;
 
+import com.github.lucasdevrj.frewgamesinterativo.jogos.Jogo;
+import com.github.lucasdevrj.frewgamesinterativo.jogos.JogoPlayStation;
+import com.github.lucasdevrj.frewgamesinterativo.loja.LojaVideoGame;
+
 public class XboxOne extends Xbox {
 
 	public XboxOne(int codigo, String nome, String modelo, String dataLancamento, String marca, float preco) {
@@ -7,6 +11,114 @@ public class XboxOne extends Xbox {
 	}
 	
 	public static void exibeJogosXboxOne() {
+		System.out.println("\n----------|JOGOS DE XBOX ONE|----------");
+		System.out.println("Opção 1 - Watch Dogs 2.");
+		System.out.println("Opção 2 - Far Cry 6.");
+		System.out.println("Opção 3 - Battlefield 2042.");
+		System.out.println("Opção 4 - Batman. Arkham Knight.");
+		System.out.println("Opção 5 - The Witcher 3.");
+		System.out.println("----------------------------------------------");
 		
+		System.out.print("Digite a opção desejada: ");
+		int opcao = entrada.nextInt();
+		
+		switch (opcao) {
+			case 1:
+				System.out.println("\nDeseja adicionar o jogo no carrinho?");
+				System.out.println("Opção 1 - Sim.");
+				System.out.println("Opção 2 - Volta para o menu principal.");
+				System.out.print("Digite sua opção: ");
+				opcao = entrada.nextInt();
+				
+				if (opcao == 1) {
+					System.out.println("\nJogo adicionado no carrinho!");
+					Jogo jogo = new JogoPlayStation(306, "Silent Hill Hd Collection", "20/03/2012", "Terror", "KONAMI", 80.00f);
+					LojaVideoGame.getProdutos().add(jogo);
+					LojaVideoGame.setPrecoTotal(LojaVideoGame.getPrecoTotal() + jogo.getPreco());
+					LojaVideoGame.exibeMenu();
+					
+				} else if (opcao == 2) {
+					LojaVideoGame.exibeMenu();
+					
+				} else {
+					System.out.println("Opção inválida!");
+					exibeJogosXbox360();
+				}
+				
+			break;
+			
+			case 2:
+				System.out.println("\nDeseja adicionar o jogo no carrinho?");
+				System.out.println("Opção 1 - Sim.");
+				System.out.println("Opção 2 - Volta para o menu principal.");
+				System.out.print("Digite sua opção: ");
+				opcao = entrada.nextInt();
+				
+				if (opcao == 1) {
+					System.out.println("\nJogo adicionado no carrinho!");
+					Jogo jogo = new JogoPlayStation(307, "Deadpool", "25/06/2013","Ação e Aventura", "Activision", 70.00f);
+					LojaVideoGame.getProdutos().add(jogo);
+					LojaVideoGame.setPrecoTotal(LojaVideoGame.getPrecoTotal() + jogo.getPreco());
+					LojaVideoGame.exibeMenu();
+					
+				} else if (opcao == 2) {
+					LojaVideoGame.exibeMenu();
+					
+				} else {
+					System.out.println("Opção inválida!");
+					exibeJogosXbox360();
+				}
+				
+			break;
+			
+			case 3:
+				
+				System.out.println("\nDeseja adicionar o jogo no carrinho?");
+				System.out.println("Opção 1 - Sim.");
+				System.out.println("Opção 2 - Volta para o menu principal.");
+				System.out.print("Digite sua opção: ");
+				opcao = entrada.nextInt();
+				
+				if (opcao == 1) {
+					System.out.println("\nJogo adicionado no carrinho!");
+					Jogo jogo = new JogoPlayStation(308, "Red Dead Redemption", "03/21/2010","Mundo Aberto", "Rockstar",  90.00f);
+					LojaVideoGame.getProdutos().add(jogo);
+					LojaVideoGame.setPrecoTotal(LojaVideoGame.getPrecoTotal() + jogo.getPreco());
+					LojaVideoGame.exibeMenu();
+					
+				} else if (opcao == 2) {
+					LojaVideoGame.exibeMenu();
+					
+				} else {
+					System.out.println("Opção inválida!");
+					exibeJogosXbox360();
+				}
+			
+			break;
+			
+			case 4:
+				
+				System.out.println("\nDeseja adicionar o jogo no carrinho?");
+				System.out.println("Opção 1 - Sim.");
+				System.out.println("Opção 2 - Volta para o menu principal.");
+				System.out.print("Digite sua opção: ");
+				opcao = entrada.nextInt();
+				
+				if (opcao == 1) {
+					System.out.println("\nJogo adicionado no carrinho!");
+					Jogo jogo = new JogoPlayStation(309, "Jogo Batman - Arkham Origins", "25/10/2013","Ação e Aventura", " WB Games Montréal", 80.00f);
+					LojaVideoGame.getProdutos().add(jogo);
+					LojaVideoGame.setPrecoTotal(LojaVideoGame.getPrecoTotal() + jogo.getPreco());
+					LojaVideoGame.exibeMenu();
+					
+				} else if (opcao == 2) {
+					LojaVideoGame.exibeMenu();
+					
+				} else {
+					System.out.println("Opção inválida!");
+					exibeJogosXbox360();
+				}
+				
+			break;
 	}
 }
